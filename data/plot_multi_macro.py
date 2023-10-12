631a179ee8886c542e76213d274e8a5282049776
cameronpoe <cameronmpoe@gmail.com>
2023-10-12 17:07:14 -0500

    spline fits of wall thickness optimization

--- data/plot_multi_macro.py
+++ data/plot_multi_macro.py
@@ -1,8 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import trapezoid
+from scipy.interpolate import splrep, BSpline, CubicSpline
 
-table_path = r'data/eff_v_angle_per_wall.npy'
+table_path = r'data/eff_v_angle_per_wall_ecomass.npy'
 phi_ind = 0
 wall_thicknesses = np.linspace(10, 200, 20, dtype=int)
 
@@ -18,11 +19,23 @@ for effs in table[:,:,0]:
     integrated_effs.append(avg_eff)
 integrated_effs = np.array(integrated_effs)
 
-fig, ax = plt.subplots()
-ax.scatter(wall_thicknesses, integrated_effs, color='black')
+print(wall_thicknesses)
+print(integrated_effs)
+spline_tuple = splrep(wall_thicknesses, integrated_effs, k=3)
+data_bspline = BSpline(*spline_tuple)
+ddata_bspline = data_bspline.derivative()
+spline_domain = np.linspace(wall_thicknesses[0], wall_thicknesses[-1], 1000)
+dcubic_spline = CubicSpline(spline_domain, ddata_bspline(spline_domain))
+extrema = dcubic_spline.solve(0, extrapolate=False)[0]
+print(extrema)
 
-ax.set_xlabel('Wall thickness (um)', fontdict=dict(size=14))
-ax.set_ylabel('Average efficiency (zenith)', fontdict=dict(size=14))
+fig, ax = plt.subplots()
+ax.scatter(wall_thicknesses, integrated_effs, color='black', label='Raw data')
+ax.plot(spline_domain, data_bspline(spline_domain), color='red', label='Spline fit')
+ax.axvline(extrema, label='Maximum: {:.2f} um'.format(extrema))
+ax.set_xlabel('Wall thickness (um)', fontdict=dict(size=12))
+ax.set_ylabel('Average efficiency (zenith)', fontdict=dict(size=12))
+ax.legend()
 ax.xaxis.set_ticks_position('both')
 ax.yaxis.set_ticks_position('both')
 plt.minorticks_on()
---
+++ data/wall_thickness_optimization/README.txt
@@ -0,0 +1,3 @@
+ecomass:                500k events per run, swept zenith 0->90 in 5deg intervals, pore size: 2mil x 2mil
+g4_glass_lead_old:      ??? events per run, swept zenith 0->90 in 5deg intervals (didn't normalize integration), pore size: 2mil x 100mill
+g4_glass_lead:          500k events per run, swept zenith 0->90 in 5deg intervals, pore size: 2mil x 2mil
