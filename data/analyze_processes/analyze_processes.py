261a52651c507d063b1aa911bf89685297b82fa4
cameronpoe <cameronmpoe@gmail.com>
2023-10-26 12:17:23 -0500

    to do process anaylis

--- data/analyze_processes/analyze_processes.py
+++ data/analyze_processes/analyze_processes.py
@@ -1,5 +1,60 @@
 import numpy as np
-from matplotlib import pyplot as plt
-import uproot 
+import os
+import uproot
+import awkward as ak
+import pickle
 
-data_file = r'raw_data/latest_run/Run_wall50_pore_50_theta0_phi0.root'
+data_directory = r'raw_data/latest_run/'
+num_histories_per_run = 250000
+wall_array = np.array([50])
+pore_array = np.array([50])
+gamma_ray_energies = np.linspace(10, 600, 60, dtype=int)
+SINGLE_ZENITH = True
+theta_increment = 5
+SINGLE_AZIMUTH = True
+phi_increment = 3
+
+
+
+
+
+if SINGLE_ZENITH:
+    theta_array = np.array([45])
+else:
+    theta_array = np.linspace(0, 90, int(90/theta_increment + 1))
+if SINGLE_AZIMUTH:
+      phi_array = np.array([0])
+else:
+    phi_array = np.linspace(0, 90, int(90/phi_increment + 1))
+
+
+proc_plot_dict = {}
+
+for file_name in os.listdir(data_directory):
+
+    file_path = data_directory + file_name
+
+    with uproot.open(file_path) as f:
+            
+        fname_components = file_name.split('_')
+        energy = int(fname_components[1][1:])
+        energy_ind = int((energy - gamma_ray_energies[0])/(gamma_ray_energies[1]-gamma_ray_energies[0]))
+        
+        pore_tree = f['pore']
+        pore_branches = pore_tree.arrays(library='ak')
+        pore_branches['CreatorProc'] = ak.str.split_pattern(pore_branches['CreatorProc'], '\n')
+        
+        labels, counts = np.unique(np.array(ak.flatten(pore_branches['CreatorProc'])), return_counts=True)
+        proc_fracs = counts/num_histories_per_run
+
+        for label, proc_frac in zip(labels, proc_fracs):
+            if label not in proc_plot_dict:
+                proc_plot_dict[label] = np.zeros_like(gamma_ray_energies, dtype=np.float64)
+            proc_plot_dict[label][energy_ind] = proc_frac
+
+save_file_path = data_directory.split('/')[-2]
+with open(save_file_path + '.pkl', 'wb') as f:
+    pickle.dump(proc_plot_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
---
+++ data/analyze_processes/plot_procs.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle
+
+pickle_object_path = r''
+gamma_ray_energies = np.linspace(10, 600, 60, dtype=int)
+
+
+
+
+with open(pickle_object_path, 'rb') as f:
+
+    proc_plot_dict = pickle.load(f)
+
+    fig, ax = plt.subplots()
+    for proc in proc_plot_dict:
+        ax.plot(gamma_ray_energies, 100*proc_plot_dict[proc], label=proc)
+        ax.scatter(gamma_ray_energies, 100*proc_plot_dict[proc], marker='.')
+    ax.legend()
+    ax.set_xlabel('Gamma ray energies (keV)', fontdict=dict(size=12.5))
+    ax.set_ylabel('Fraction of interactions (%)', fontdict=dict(size=12.5))
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    plt.minorticks_on()
+    plt.show()
+
