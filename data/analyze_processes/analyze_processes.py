781638eb5467c7b4d1cd51d9b634a0a5c21571c8
Cameron Poe <cameronmpoe@gmail.com>
2023-10-26 08:12:39 -0500

    finished peek

--- data/analyze_processes/analyze_processes.py
+++ data/analyze_processes/analyze_processes.py
@@ -4,7 +4,7 @@ import uproot
 import awkward as ak
 import pickle
 
-data_directory = r'../../raw_data/b33_procs/'
+data_directory = r'../../raw_data/peek_procs/'
 num_histories_per_run = 500000
 wall_array = np.array([50])
 pore_array = np.array([50])
