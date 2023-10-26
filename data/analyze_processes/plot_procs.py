82e7b03b5d3c644873b1c004c22c7bbb413cc65a
cameronpoe <cameronmpoe@gmail.com>
2023-10-26 14:26:57 -0500

    new figs

--- data/analyze_processes/plot_procs.py
+++ data/analyze_processes/plot_procs.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pickle
 
-pickle_object_path = r'data/analyze_processes/peek_procs.pkl'
+pickle_object_path = r'data/analyze_processes/g4_glass_lead_procs.pkl'
 gamma_ray_energies = np.linspace(10, 600, 60, dtype=int)
 
 
@@ -22,5 +22,19 @@ with open(pickle_object_path, 'rb') as f:
     ax.xaxis.set_ticks_position('both')
     ax.yaxis.set_ticks_position('both')
     plt.minorticks_on()
+
+    fig, ax = plt.subplots()
+    for proc in proc_plot_dict:
+        if proc == 'overall':
+            continue
+        ax.plot(gamma_ray_energies, 100*proc_plot_dict[proc]/proc_plot_dict['overall'], label=proc)
+        ax.scatter(gamma_ray_energies, 100*proc_plot_dict[proc]/proc_plot_dict['overall'], marker='.')
+    ax.legend()
+    ax.set_xlabel('Gamma ray energies (keV)', fontdict=dict(size=12.5))
+    ax.set_ylabel('Fraction of converted electrons (%)', fontdict=dict(size=12.5))
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    plt.minorticks_on()
     plt.show()
+
 
