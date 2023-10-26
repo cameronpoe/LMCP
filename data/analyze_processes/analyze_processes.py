770fef9f83f739b2708ac08f7284911a73d6b980
cameronpoe <cameronmpoe@gmail.com>
2023-10-26 14:15:57 -0500

    removed all the printing stuff

--- data/analyze_processes/analyze_processes.py
+++ data/analyze_processes/analyze_processes.py
@@ -49,14 +49,9 @@ for file_num, file_name in enumerate(os.listdir(data_directory)):
         intermediate_dict = {}
         labels = np.unique(np.array(ak.flatten(pore_branches['CreatorProc'])))
         for label in labels:
-            print('New label: ' + str(label))
             intermediate_dict[label] = len(pore_branches['CreatorProc'][ak.any(pore_branches['CreatorProc'] == label, axis=1)])
 
-        print(intermediate_dict)
-
         overall_events_converted = len(pore_branches['EventNumber'])
-
-        print('Total converted' + str(overall_events_converted))
         intermediate_dict['overall'] = overall_events_converted
 
         for label in intermediate_dict:
