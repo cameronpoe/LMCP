bfd68edb9c7ffdb169646db786e8b25e7a48116d
Cameron Poe <cameronmpoe@gmail.com>
2023-10-30 09:56:28 -0500

    huh?

--- data/analyze_multi_macro.py
+++ data/analyze_multi_macro.py
@@ -20,24 +20,30 @@ if SINGLE_AZIMUTH:
       phi_array = np.array([0])
 
 eff_v_angle = np.empty((len(wall_array), len(pore_array), len(theta_array), len(phi_array)))
-
+print(eff_v_angle.shape)
 for file_name in os.listdir(data_directory):
 
     file_path = data_directory + file_name
 
     with uproot.open(file_path) as f:
-            
-            fname_components = file_name.split('_')
-            #print(fname_components)
-            wall_ind = int((int(fname_components[2][1:])-wall_array[0])/np.diff(wall_array)[0])
-            pore_ind = int((int(fname_components[3][1:])-pore_array[0])/np.diff(pore_array)[0])
-            theta_ind = int(fname_components[4][5:])
-            phi_ind = int(fname_components[5][3:-5])
+        
+        fname_components = file_name.split('_')
+        #print(fname_components)
+        wall_ind = int((int(fname_components[2][1:])-wall_array[0])/np.diff(wall_array)[0])
+        pore_ind = int((int(fname_components[3][1:])-pore_array[0])/np.diff(pore_array)[0])
+        theta_ind = int(fname_components[4][5:])
+        phi_ind = int(fname_components[5][3:-5])
 
-            num_events_reached_pore = len(f['pore']['EventNumber'].array(library='np'))
-                  
-            eff = num_events_reached_pore/num_histories_per_run
-            #print(f'{wall_ind} {pore_ind} {theta_ind} {phi_ind}')
-            eff_v_angle[wall_ind][pore_ind][theta_ind][phi_ind] = eff
+        num_events_reached_pore = len(f['pore']['EventNumber'].array(library='np'))
+
+        eff = num_events_reached_pore/num_histories_per_run
+        eff_v_angle[wall_ind][pore_ind][theta_ind][phi_ind] = eff
+        if wall_ind == 14 and pore_ind == 14:
+            #print(num_events_reached_pore)
+            print(eff_v_angle[wall_ind,pore_ind,:,phi_ind])
+        #if wall_ind == 14:
+            #print(f'{num_events_reached_pore} hmm')
+
+print(eff_v_angle[14,14,:,:])
 
 np.save(data_directory.split('/')[-2], eff_v_angle)
