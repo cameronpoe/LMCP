89cad0249aa64d70137d54654b8151500f8feb31
cameronpoe <cameronmpoe@gmail.com>
2023-11-16 10:30:29 -0600

    single lamina thickness analysis working?

--- data/analyze_multi_macro.py
+++ data/analyze_multi_macro.py
@@ -1,10 +1,11 @@
 import numpy as np
 import os
 import uproot
+import awkward as ak
 
 data_directory = r'../raw_data/ecomass_varporewall/'
-num_histories_per_run = 250000
-wall_array = np.linspace(5, 145, 15, dtype=int)
+num_histories_per_run = 500000
+wall_array = np.linspace(5, 195, 39, dtype=int)
 #wall_array = np.array([50])
 pore_array = np.linspace(5, 145, 15, dtype=int)
 #pore_array = np.array([50])
@@ -12,6 +13,7 @@ theta_increment = 5
 phi_increment = 3
 SINGLE_AZIMUTH = True
 SAVE_FOR_PET = False
+SINGLE_LAMINA_THICKNESS = True
 
 
 
@@ -22,11 +24,14 @@ theta_array = np.linspace(0, 90, int(90/theta_increment + 1))
 phi_array = np.linspace(0, 90, int(90/phi_increment + 1))
 if SINGLE_AZIMUTH:
       phi_array = np.array([0])
-
-eff_v_angle = np.empty((len(wall_array), len(pore_array), len(theta_array), len(phi_array)))
+if SINGLE_LAMINA_THICKNESS:
+     pore_array = np.array([0])
 if SAVE_FOR_PET:
      eff_v_angle = np.empty((len(theta_array), len(phi_array)))
-print(eff_v_angle.shape)
+else:
+     eff_v_angle = np.empty((len(wall_array), len(pore_array), len(theta_array), len(phi_array)))
+print(f'eff_v_angle shape: {eff_v_angle.shape}')
+
 
 for file_name in os.listdir(data_directory):
 
@@ -35,15 +40,22 @@ for file_name in os.listdir(data_directory):
     with uproot.open(file_path) as f:
 
         fname_components = file_name.split('_')
-        if not SAVE_FOR_PET:
+        if SINGLE_LAMINA_THICKNESS:
+            wall_ind = int((int(fname_components[2][1:])-wall_array[0])/np.diff(wall_array)[0])
+            pore_ind = 0
+        elif not SAVE_FOR_PET:
             wall_ind = int((int(fname_components[2][1:])-wall_array[0])/np.diff(wall_array)[0])
             pore_ind = int((int(fname_components[3][1:])-pore_array[0])/np.diff(pore_array)[0])
         theta_ind = int(fname_components[4][5:])
         phi_ind = int(fname_components[5][3:-5])
 
-        num_events_reached_pore = len(f['pore']['EventNumber'].array(library='np'))
+        pore_tree = f['pore']
+        pore_branches = pore_tree.arrays(library='ak')
 
-        eff = num_events_reached_pore/num_histories_per_run
+        # num_events_reached_pore = len(f['pore']['EventNumber'].array(library='np'))
+        num_electrons_reached_pore = len(pore_branches['EventNumber', pore_branches['PDGID'] == 11])
+
+        eff = num_electrons_reached_pore/num_histories_per_run
         if SAVE_FOR_PET:
              eff_v_angle[theta_ind][phi_ind] = eff
         else:
