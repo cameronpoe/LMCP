940f35d8c61580048cc88a33c85685b767de6950
iang2123 <iangoldberg@uchicago.edu>
2024-06-06 11:45:41 -0500

    40 micron b33 run

--- macros/st_var_lmcp_var_source_bash_macro_maker.py
+++ macros/st_var_lmcp_var_source_bash_macro_maker.py
@@ -15,9 +15,9 @@ lmcp_dimensions = np.array([2.54, 2.54, 2.54])              # cm
 LINK_WALL_AND_PORE = True
 # wall_thicknesses = np.linspace(5,95,19,dtype=int)
 # wall_thicknesses = np.linspace(5, 195, 39, dtype=int)      # um (Wall thickness is BETA)
-wall_thicknesses = np.array([20])                             # um
+wall_thicknesses = np.array([40])                             # um
 # pore_widths = np.linspace(25, 100, 4, dtype=int)             # um (Pore widths is GAMMA)
-pore_widths = np.array([20])				   # um
+pore_widths = np.array([40])				   # um
 
 
 # gamma_energies = np.linspace(10, 600, 60, dtype=int)          # keV
